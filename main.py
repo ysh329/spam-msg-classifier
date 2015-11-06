9de995c8234c4010d63936a886c2c391fa9636af
yuens <ysh329@sina.com>
2015-11-06 22:07:51 +0800

    PySpark can import into python env. and parse-config module is ok.

--- main.py
+++ main.py
@@ -13,9 +13,32 @@ __author__ = 'yuens'
 from myclass.class_create_database_table import *
 from myclass.class_read_text_to_database import *
 import os.path
+import ConfigParser
 ################################ PART3 MAIN ###########################################
 def main():
-    pass
+
+    conf = ConfigParser.ConfigParser()
+    conf.read("./config.ini")
+    appName = conf.get("basic", "appName")
+    logging.info("appName: {appName}".format(appName = appName))
+
+    database_name = conf.get("database", "database_name")
+    database_password = conf.get("database", "database_password")
+    message_table_name = conf.get("database", "message_table_name")
+    word_table_name = conf.get("database", "word_table_name")
+
+    logging.info("database_name: {database_name}".format(database_name = database_name))
+    logging.info("database_password: {database_password}".format(database_password = database_password))
+    logging.info("message_table_name: {message_table_name}".format(message_table_name = message_table_name))
+    logging.info("word_table_name: {word_table_name}".format(word_table_name = word_table_name))
+
+    train_data_dir = conf.get("data", "train_data_dir")
+    test_data_dir = conf.get("data", "test_data_dir")
+    stopword_data_dir = conf.get("data", "stopword_data_dir")
+
+    logging.info("train_data_dir: {train_data_dir}".format(train_data_dir = train_data_dir))
+    logging.info("test_data_dir: {test_data_dir}".format(test_data_dir = test_data_dir))
+    logging.info("stopword_data_dir: {stopword_data_dir}".format(stopword_data_dir = stopword_data_dir))
 
 ################################ PART4 EXECUTE ##################################
 if __name__ == "__main__":
