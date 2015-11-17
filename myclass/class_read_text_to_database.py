09c886fec619df25142ca942fc70121829ad1dc0
yuens <ysh329@sina.com>
2015-11-17 21:37:51 +0800

    Modified three parts below: 1. Delete the SparkContext generate part, use the input variable "pyspark_sc" generated by class "SparkCreate" instead. 2. Because of large RDD from large data set, so I randomSplit the RDD into 8 RDD to accomplish sql insert task with low JVM storage memory. 3. Delete the older version code, which doesn't use spark.

--- myclass/class_read_text_to_database.py
+++ myclass/class_read_text_to_database.py
@@ -21,7 +21,7 @@ from pyspark import SparkContext, SparkConf, StorageLevel
 ################################### PART2 CLASS && FUNCTION ###########################
 class ReadText2DB(object):
 
-    def __init__(self, database_name, train_data_dir, stopword_data_dir, pyspark_app_name):
+    def __init__(self, database_name, train_data_dir, stopword_data_dir, pyspark_sc):
         self.start = time.clock()
 
         logging.basicConfig(level = logging.INFO,
@@ -62,9 +62,7 @@ class ReadText2DB(object):
 
         # Configure Spark
         try:
-            conf = SparkConf().setAppName(pyspark_app_name)
-            conf = conf.setMaster("local[8]")
-            self.sc = SparkContext(conf = conf)
+            self.sc = pyspark_sc
             logging.info("Start pyspark successfully.")
         except Exception as e:
             logging.error("Fail in starting pyspark.")
@@ -73,16 +71,30 @@ class ReadText2DB(object):
 
 
     def __del__(self):
-        self.train_f.close()
-        self.stopword_f.close()
-        self.con.close()
+        try:
+            self.train_f.close()
+            logging.info("Success in closing training data file.")
+        except Exception as e:
+            logging.error("Fail in closing training data file.")
+            logging.error(e)
 
-        logging.info("Success in closing file.")
-        logging.info("Success in quiting MySQL.")
-        logging.info("END.")
+        try:
+            self.stopword_f.close()
+            logging.info("Success in closing stop word data file.")
+        except Exception as e:
+            logging.error("Fail in closing stop word data file.")
+            logging.error(e)
 
+        try:
+            self.con.close()
+            logging.info("Success in quiting MySQL.")
+        except MySQLdb.Error, e:
+            logging.error("Fail in quiting MySQL.")
+            logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
+
+        logging.info("END CLASS {class_name}.".format(class_name = ReadText2DB.__name__))
         self.end = time.clock()
-        logging.info("The function run time is : %.03f seconds" % (self.end - self.start))
+        logging.info("The class {class_name} run time is : {delta_time} seconds".format(class_name = ReadText2DB.__name__, delta_time = self.end))
 
 
 
@@ -224,207 +236,35 @@ class ReadText2DB(object):
 
         message_insert_sql_rdd.cache()
         message_insert_sql_rdd_list= message_insert_sql_rdd.randomSplit([1, 1, 1, 1, 1, 1, 1, 1])
-        message_insert_sql_list = message_insert_sql_rdd_list[0].collect()# + rdd2.collect() + rdd3.collect() + rdd4.collect()
-        logging.info("len(message_insert_sql_list):{0}".format(len(message_insert_sql_list)))
-
-        success_insert = 0
-        failure_insert = 0
 
         cursor = self.con.cursor()
-        for idx in xrange(len(message_insert_sql_list)):
-            sql = message_insert_sql_list[idx]
-            if (idx % 10000 == 0 and idx > 9998) or (idx == len(message_insert_sql_list) -1):
-                logging.info("==========={0}th element in message_insert_sql_list===========".format(idx))
-                logging.info("sql_execute_index:{idx}, finish rate:{rate}".format(idx = idx, rate = float(idx+1)/ len(message_insert_sql_list)))
-                logging.info("success_rate:{success_rate}".format(success_rate = success_insert / float(success_insert + failure_insert)))
-                logging.info("success_insert:{success}, failure_insert:{failure}".format(success = success_insert, failure = failure_insert))
+        for rdd_idx in xrange(len(message_insert_sql_rdd_list)):
+            message_insert_sql_list = message_insert_sql_rdd_list[rdd_idx].collect()# + rdd2.collect() + rdd3.collect() + rdd4.collect()
+            logging.info("len(message_insert_sql_list):{0}".format(len(message_insert_sql_list)))
 
-            try:
-                cursor.execute(sql)
-                self.con.commit()
-                success_insert = success_insert + 1
-            except MySQLdb.Error, e:
-                self.con.rollback()
-                logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
-                logging.error("error SQL:{0}".format(sql))
-                failure_insert = failure_insert + 1
+            success_insert = 0
+            failure_insert = 0
+
+            for idx in xrange(len(message_insert_sql_list)):
+                sql = message_insert_sql_list[idx]
+                if (idx % 10000 == 0 and idx > 9998) or (idx == len(message_insert_sql_list) -1):
+                    logging.info("==========={0}th element in message_insert_sql_list===========".format(idx))
+                    logging.info("sql_execute_index:{idx}, finish rate:{rate}".format(idx = idx, rate = float(idx+1)/ len(message_insert_sql_list)))
+                    logging.info("success_rate:{success_rate}".format(success_rate = success_insert / float(success_insert + failure_insert)))
+                    logging.info("success_insert:{success}, failure_insert:{failure}".format(success = success_insert, failure = failure_insert))
+
+                try:
+                    cursor.execute(sql)
+                    self.con.commit()
+                    success_insert = success_insert + 1
+                except MySQLdb.Error, e:
+                    self.con.rollback()
+                    logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
+                    logging.error("error SQL:{0}".format(sql))
+                    failure_insert = failure_insert + 1
         cursor.close()
-        """
-        def sql_insert(self, cursor, sql):
-            try:
-                cursor.execute(sql)
-                self.con.commit()
-            except MySQLdb.Error, e:
-                self.con.rollback()
-                logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
-                logging.error("error SQL:{0}".format(sql))
-
-
-        cursor = self.con.cursor()
-        message_insert_sql_rdd.foreach(lambda sql: sql_insert(self, cursor, sql))
-        self.con.commit()
-        cursor.close()
-        """
-
-
-
-
-    # (id, is_train, true_label, word_num, content, split_result_string, split_result_num, split_result_clean_num)
-    def read_text_into_meta_data(self, data_set):
-        logging.info("")
-        try:
-            train_line_list = self.train_f.readlines()
-            logging.info("Success in reading file to variable.")
-            logging.info("type(train_line_list): %s" % type(train_line_list))
-            logging.info("len(train_line_list): %s" % len(train_line_list))
-            logging.info("train_line_list[0]: %s" % train_line_list[0])
-            logging.info("train_line_list[len(train_line_list)-1]: %s" % train_line_list[len(train_line_list)-1])
-        except Exception as e:
-            logging.error(e)
-            logging.error("Can't read training data set.\nTermination.")
-            return
-
-        train_data_triple_2d_list = map(lambda line: line.split("\t"), train_line_list)
-        logging.info("len(train_data_triple_2d_list): %s." % len(train_data_triple_2d_list))
-        logging.info("train_data_triple_2d_list[0]: %s." % train_data_triple_2d_list[0])
-        logging.info("len(train_data_triple_2d_list[0]): %s." % len(train_data_triple_2d_list[0][2]))
-        #for i in train_data_triple_2d_list[0]: print i
-        logging.info("train_data_triple_2d_list[len(train_data_triple_2d_list)-1]: %s." % train_data_triple_2d_list[len(train_data_triple_2d_list)-1])
-        #for i in train_data_triple_2d_list[len(train_data_triple_2d_list)-1]: print i
-
-        # clean data
-        # (id, is_train, true_label, word_num, content, split_result_string, split_result_num, split_result_clean_string, split_result_clean_num)
-        # id
-        id_list = map(lambda record: record[0].strip(), train_data_triple_2d_list)
-        # is_train
-        is_train_list = map(lambda record: 1 if len(record) == 3 else 0, train_data_triple_2d_list)
-        # true_label
-        true_label_list = map(lambda record: record[1].strip(), train_data_triple_2d_list)
-        # word_num
-        word_num_list = map(lambda record: len(record[2].decode("utf8")), train_data_triple_2d_list)
-        # content
-        content_list = map(lambda record: record[2].strip().lower(), train_data_triple_2d_list)
-
-        split_result_2d_list = map(lambda message: list(jieba.cut(message)), content_list)
-        # split_result_string
-        split_result_string_list = map(lambda result_list: "///".join(result_list), split_result_2d_list)
-        # split_result_num
-        split_result_num_list = map(lambda result_list: len(result_list), split_result_2d_list)
-
-        logging.info("len(id_list): %s." % len(id_list))
-        logging.info("type(id_list[0]): %s." % type(id_list[0]))
-        logging.info("id_list[0]: %s." % id_list[0])
-        logging.info("id_list[len(id_list)-1]: %s." % id_list[len(id_list)-1])
-        logging.info("")
-        logging.info("len(is_train_list): %s." % len(is_train_list))
-        logging.info("type(is_train_list[0]): %s." % type(is_train_list[0]))
-        logging.info("is_train_list[0]: %s." % is_train_list[0])
-        logging.info("is_train_list[len(is_train_list)-1]: %s." % is_train_list[len(is_train_list)-1])
-        logging.info("")
-        logging.info("len(true_label_list): %s." % len(true_label_list))
-        logging.info("type(true_label_list[0]): %s." % type(true_label_list[0]))
-        logging.info("true_label_list[0]: %s." % true_label_list[0])
-        logging.info("true_label_list[len(true_label_list)-1]: %s." % true_label_list[len(true_label_list)-1])
-        logging.info("")
-        logging.info("len(content_list): %s." % len(content_list))
-        logging.info("type(content_list[0]): %s." % type(content_list[0]))
-        logging.info("content_list[0]: %s." % content_list[0])
-        logging.info("content_list[len(content_list)-1]: %s." % content_list[len(content_list)-1])
-        logging.info("")
-        logging.info("len(word_num_list): %s." % len(word_num_list))
-        logging.info("type(word_num_list[0]): %s." % type(word_num_list[0]))
-        logging.info("word_num_list[0]: %s." % word_num_list[0])
-        logging.info("word_num_list[len(word_num_list)-1]: %s." % word_num_list[len(word_num_list)-1])
-        logging.info("")
-        logging.info("len(split_result_string_list): %s." % len(split_result_string_list))
-        logging.info("type(split_result_string_list[0]): %s." % type(split_result_string_list[0]))
-        logging.info("split_result_string_list[0]: %s." % split_result_string_list[0])
-        logging.info("split_result_string_list[len(split_result_string_list)-1]: %s." % split_result_string_list[len(split_result_string_list)-1])
-        logging.info("")
-        logging.info("len(split_result_2d_list): %s." % len(split_result_2d_list))
-        logging.info("type(split_result_2d_list[0]): %s." % type(split_result_2d_list[0]))
-        logging.info("split_result_2d_list[0]: %s." % split_result_2d_list[0])
-        logging.info("split_result_2d_list[len(split_result_2d_list)-1]: %s." % split_result_2d_list[len(split_result_2d_list)-1])
-        logging.info("")
-        logging.info("len(split_result_num_list): %s." % len(split_result_num_list))
-        logging.info("type(split_result_num_list[0]): %s." % type(split_result_num_list[0]))
-        logging.info("split_result_num_list[0]: %s." % split_result_num_list[0])
-        logging.info("split_result_num_list[len(split_result_num_list)-1]: %s." % split_result_num_list[len(split_result_num_list)-1])
-
-        return id_list, is_train_list, true_label_list, word_num_list, content_list, split_result_string_list, split_result_num_list, split_result_2d_list
-
-
-
-    def save_meta_data_to_database(self, database_name, table_name, id_list, is_train_list, true_label_list, word_num_list, content_list, split_result_string_list, split_result_num_list):
-        logging.info("")
-        cursor = self.con.cursor()
-        sqls = ['USE %s' % database_name, 'SET NAMES UTF8']
-        sqls.append("ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name)
-
-        for id_idx in xrange(len(id_list)):
-            id = id_list[id_idx]
-            is_train = is_train_list[id_idx]
-            true_label = true_label_list[id_idx]
-            word_num = word_num_list[id_idx]
-            content = content_list[id_idx].replace("'", '"').replace("\\", "")
-            split_result_string = split_result_string_list[id_idx].encode('utf8').replace("'", '"').replace("\\", "")
-            split_result_num = split_result_num_list[id_idx]
-
-            sqls.append("""INSERT INTO %s.%s(id, is_train, true_label, word_num, content, split_result_string, split_result_num) VALUES (%s, %s, %s, %s, '%s', '%s', %s)""" % (database_name, table_name, id, is_train, true_label, word_num, content, split_result_string, split_result_num))
-
-        logging.info("len(sqls): %s." % len(sqls))
-        logging.info("sqls[10]: %s." % sqls[10])
-        logging.info("type(sqls[10]): %s." % type(sqls[10]))
-        logging.info("sqls[len(sqls)-1]: %s." % sqls[len(sqls)-1])
-
-        '''
-        try:
-            map(lambda sql: cursor.execute(sql), sqls)
-            self.con.commit()
-        except Exception as e:
-            logging.error(e)
-            logging.error(sql)
-        '''
-        success_insert = 0
-        failure_insert = 0
-        for sql_idx in xrange(len(sqls)):
-            sql = sqls[sql_idx]
-            if sql_idx % 1000 == 0:
-                logging.info("sql_idx: %s." % sql_idx)
-
-            try:
-                cursor.execute(sql)
-                self.con.commit()
-                success_insert = success_insert + 1
-            except MySQLdb.Error, e:
-                self.con.rollback()
-                failure_insert = failure_insert + 1
-                logging.error("Error SQL[sql_idx: %s]: %s." % (sql_idx, sql))
-                logging.error("MySQL Error %d: %s." % (e.args[0], e.args[1]))
-
-        logging.info("success_insert: %s." % success_insert)
-        logging.info("failure_insert: %s." % failure_insert)
-        logging.info("summation insert: %s." % (success_insert + failure_insert))
-        cursor.close()
-
-
-
-
-    """
-    def read_text_into_clean_data(self, split_result_2d_list):
-
-        split_result_1d_list = list(set(sum(split_result_2d_list, [])))
-        logging.info("")
-        logging.info("len(split_result_1d_list): %s." % len(split_result_1d_list))
-        logging.info("type(split_result_1d_list): %s." % type(split_result_1d_list))
-        logging.info("split_result_1d_list[0]: %s." % split_result_1d_list[0])
-        logging.info("type(split_result_1d_list[0]): %s." % type(split_result_1d_list[0]))
-        logging.info("split_result_1d_list[len(split_result_1d_list)-1]: %s." % split_result_1d_list[len(split_result_1d_list)-1])
-        logging.info("type(split_result_1d_list[len(split_result_1d_list)-1]): %s." % type(split_result_1d_list[len(split_result_1d_list)-1]))
-    """
-
 ################################### PART3 CLASS TEST ##################################
-
+"""
 # initial parameters
 database_name = "messageDB"
 table_name_list = ["message_table", "word_table"]
@@ -440,14 +280,14 @@ Reader = ReadText2DB(database_name = database_name,
                      train_data_dir = train_data_dir,
                      stopword_data_dir = stopword_data_dir,
                      pyspark_app_name = pyspark_app_name)
-"""
+
 #id_list, is_train_list, true_label_list, word_num_list, content_list, split_result_string_list, split_result_num_list, split_result_2d_list = Reader.read_text_into_meta_data()
 #Reader.save_meta_data_to_database(database_name, table_name_list[0], id_list, is_train_list, true_label_list, word_num_list, content_list, split_result_string_list, split_result_num_list)
 
 #Reader.read_text_into_clean_data(split_result_2d_list)
-Reader.save_stopword_to_database(database_name = database_name,
-                             table_name_list = table_name_list)
-"""
+#Reader.save_stopword_to_database(database_name = database_name,
+#                             table_name_list = table_name_list)
+
 
 cleaned_and_processed_train_data_rdd = Reader.read_train_data(train_data_dir = train_data_dir,
                                                               stopword_data_dir = stopword_data_dir)
@@ -455,3 +295,4 @@ message_insert_sql_rdd = Reader.message_insert_sql_generator(database_name = dat
                                     message_table_name = message_table_name,
                                     cleaned_and_processed_train_data_rdd = cleaned_and_processed_train_data_rdd)
 Reader.save_train_data_to_database(message_insert_sql_rdd = message_insert_sql_rdd)
+"""
